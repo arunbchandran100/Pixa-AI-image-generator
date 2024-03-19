5c4607eb96c0d652de0c35ad30e7ddc8af668c60
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2024-03-19 21:42:43 +0530

    added footer

--- react/pixa-ai/src/Home.tsx
+++ react/pixa-ai/src/Home.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './Homestyles.css'; // Import your CSS file
-import photo1 from './assets/images/photo1.jpg';
-import photo2 from './assets/images/photo2.jpg';
-import photo3 from './assets/images/photo3.jpg';
-import photo4 from './assets/images/photo4.jpg';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate instead of useHistory
+import React from "react";
+import "./Homestyles.css"; // Import your CSS file
+import photo1 from "./assets/images/photo1.jpg";
+import photo2 from "./assets/images/photo2.jpg";
+import photo3 from "./assets/images/photo3.jpg";
+import photo4 from "./assets/images/photo4.jpg";
+import { useNavigate } from "react-router-dom"; // Import useNavigate instead of useHistory
 
 interface PhotoOptionProps {
   photo: string;
@@ -14,15 +14,17 @@ const PhotoOption: React.FC<PhotoOptionProps> = ({ photo }) => {
   const navigate = useNavigate(); // Use useNavigate hook
 
   const handleClick = () => {
-    navigate('/upload'); // Navigate to the upload page when clicked
+    navigate("/upload"); // Navigate to the upload page when clicked
   };
 
   return (
-    <div className="photo-option" onClick={handleClick}> {/* Add onClick handler to the container */}
+    <div className="photo-option" onClick={handleClick}>
+      {" "}
+      {/* Add onClick handler to the container */}
       <img src={photo} alt="Photo" />
     </div>
   );
-}
+};
 
 function Home() {
   const photos = [photo1, photo2, photo3, photo4];
@@ -38,7 +40,9 @@ function Home() {
       <div className="generator-text">Free AI Image Generator</div>
 
       {/* Description */}
-      <div className="description">Choose the styles from below to edit your photos</div>
+      <div className="description">
+        Choose the styles from below to edit your photos
+      </div>
 
       {/* Photo options container */}
       <div className="photo-options-container">
@@ -47,6 +51,13 @@ function Home() {
           <PhotoOption key={index} photo={photo} />
         ))}
       </div>
+      {/* Footer */}
+      <footer className="footer">
+        <div>About</div>
+        <div>Services</div>
+        <div>Contact</div>
+        {/* Add other basic things */}
+      </footer>
     </div>
   );
 }
