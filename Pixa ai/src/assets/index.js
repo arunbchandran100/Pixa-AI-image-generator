b5c022c755cc1eb6c8ef1aaf97c72700afd0d2cb
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2024-03-28 19:18:12 +0530

    logo updated

--- Pixa ai/src/assets/index.js
+++ Pixa ai/src/assets/index.js
@@ -4,7 +4,7 @@ import binance from "./binance.png";
 import card from "./1st card.png";
 import coinbase from "./coinbase.png";
 import dropbox from "./dropbox.png";
-import logo from "./cropped-logorexa.png";
+import logo from "./pixa ratio logo1.png";
 import quotes from "./quotes.svg";
 import robot from "./robot.png";
 import send from "./Send.svg";
