3ca799e29f6033f376d1593f11f5708ed36cbc71
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2024-05-23 00:15:19 +0530

    updated

--- Pixa ai/vite.config.js
+++ Pixa ai/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vPixa-AI-image-generator/", 
+  base: "/Pixa-AI-image-generator/", 
 })
